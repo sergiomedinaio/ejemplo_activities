9509df9ce0300d9f3c46355f5542eceed125e19d
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-18 22:42:26 -0300

    feat: update main activity and add second activity

---
+++ app/src/main/java/ar/edu/davinci/ejemplo_activities/ActivityName.java
@@ -0,0 +1,14 @@
+package ar.edu.davinci.ejemplo_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+    }
+}
